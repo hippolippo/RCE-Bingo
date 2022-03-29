9e8f6ca1f5b2182d36ffcdb7ceaea2bb99adfd21
Hippolippo <joelhaftel@gmail.com>
2022-03-29 16:18:58 -0400

    Added support for a free tile

--- generator.py
+++ generator.py
@@ -9,12 +9,17 @@ def generate_board():
         settings = load(specs)
     tile_count = settings["dimensions"][0] * settings["dimensions"][1]
     tiles_files = os.listdir("tiles")
+    if settings["free-tile"]["activated"] and settings["free-tile"]["image"] in tiles_files:
+        tiles_files.remove(settings["free-tile"]["image"])
     tiles_files = sample(tiles_files, tile_count)
     image = Image.open("backdrops/background.jpg")
     counter = 0
     for y in range(settings["dimensions"][1]):
         for x in range(settings["dimensions"][0]):
             tile = Image.open(os.path.join("tiles", tiles_files[counter]))
+            if settings["free-tile"]["activated"] and x == settings["free-tile"]["position"][0] and\
+                    y == settings["free-tile"]["position"][1]:
+                tile = Image.open(os.path.join("tiles", settings["free-tile"]["image"]))
             coordinates = [settings["top-left"][0] + settings["offset"][0] * x,
                            settings["top-left"][1] + settings["offset"][1] * y]
             image.paste(tile, (coordinates[0], coordinates[1]))
