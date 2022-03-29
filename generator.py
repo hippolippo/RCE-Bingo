2ba909c20f5161dc81275d2427835fdf5aad3b80
Hippolippo <joelhaftel@gmail.com>
2022-03-29 17:05:55 -0400

    Added support for adding weights to certain tiles

--- generator.py
+++ generator.py
@@ -1,23 +1,48 @@
-from PIL import Image, ImageDraw, ImageFilter
+from PIL import Image
 from json import load
 import os
-from random import sample
+from random import choices, sample
+
+
+def wsample(population, k, weights):
+    """
+    similar to sample function but takes a weights parameter
+    :param population: iterable object
+    :param k: integer number of results
+    :param weights: iterable object same size as population
+    :return: tuple with results
+    """
+    if len([x for x in weights if x != weights[0]]) == 0:
+        return sample(population, k)
+    population = [i for i in population]
+    result = tuple()
+    for n in range(k):
+        picked = choices([i for i in range(len(population))], weights)[0]
+        weights.pop(picked)
+        result += (population.pop(picked),)
+    return result
 
 
 def generate_board():
     with open("board_specs.json", "r") as specs:
         settings = load(specs)
+    with open("weights.json", "r") as specs:
+        weight_settings = load(specs)
     tile_count = settings["dimensions"][0] * settings["dimensions"][1]
     tiles_files = os.listdir("tiles")
+    # remove free tile from pick-able tiles
     if settings["free-tile"]["activated"] and settings["free-tile"]["image"] in tiles_files:
         tiles_files.remove(settings["free-tile"]["image"])
-    tiles_files = sample(tiles_files, tile_count)
+    weights = [(weight_settings["default-weight"] if tile not in weight_settings["overrides"]
+                else weight_settings["overrides"][tile]) for tile in tiles_files]
+    tiles_files = wsample(tiles_files, tile_count, weights)
     image = Image.open("backdrops/background.jpg")
     counter = 0
     for y in range(settings["dimensions"][1]):
         for x in range(settings["dimensions"][0]):
             tile = Image.open(os.path.join("tiles", tiles_files[counter]))
-            if settings["free-tile"]["activated"] and x == settings["free-tile"]["position"][0] and\
+            # use free tile instead if in the free tile position
+            if settings["free-tile"]["activated"] and x == settings["free-tile"]["position"][0] and \
                     y == settings["free-tile"]["position"][1]:
                 tile = Image.open(os.path.join("tiles", settings["free-tile"]["image"]))
             coordinates = [settings["top-left"][0] + settings["offset"][0] * x,
